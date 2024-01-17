22be9107fd6c3f8fbdfa7ec7253877e34954c736
rutuu <rutuu@192.168.0.109>
2024-01-17 01:38:54 +0100

    First commit.

---
+++ demo1/src/package_src/Addition.java
@@ -0,0 +1,18 @@
+package package_src;
+public class Addition 
+{
+	public static int add(int a,int b)
+	{
+		return a+b;  
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		int a = 14;
+		int b = 5;
+		int answer;
+		answer = add(a,b);
+		System.out.println(answer);
+	}
+
+}
+ 
